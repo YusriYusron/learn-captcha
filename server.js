0d029df84b49497500818434e75427dfb086a03b
Yusri Yusron <yusriyusron0530@gmail.com>
2020-01-15 10:16:09 +0700

    Rendering Captcha from Google

--- server.js
+++ server.js
@@ -11,6 +11,31 @@ app.get('/', (request, response) => {
     response.sendFile(__dirname + '/views/index.html')
 })
 
+app.post('/subscribe', (request, response) => {
+    if (request.body.captcha === undefined || request.body.captcha === '' || request.body.captcha === null){
+        return response.json({"success": false, "message": "Please select captcha"})
+    }
+
+    //Secret Key
+    const secretKey = '6Le9dM8UAAAAANHnNPbLES6vV0bBXqWcNru-5h5A'
+
+    //Verify URL
+    const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${request.body.captcha}&remoteip=${request.connection.remoteAddress}`
+
+    //Make Request to VerifyURL
+    request(verifyUrl, (err, response, body) => {
+        body = JSON.parse(body)
+
+        //If Not Successfully
+        if (body.success !== undefined && !body.success){
+            return response.json({"success": false, "message": "Failed captcha verification"})
+        }
+
+        //If Successfully
+        return response.json({"success": true, "message": "Captcha passed"})
+    })
+})
+
 app.listen(3000, () => {
     console.log('Server started on port 3000')
 })
